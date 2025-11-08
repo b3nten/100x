1235290f51c17ce568be80bbf472d2214701e77d
benton <ben@benten.ca>
2025-11-07 20:08:04 -0800

    cleanup

--- react/src/router.ts
+++ react/src/router.ts
@@ -1,67 +1,66 @@
 import {
-  createContext,
-  createElement,
-  type PropsWithChildren,
-  use,
-  useEffect,
-  useState,
+	createContext,
+	createElement,
+	type PropsWithChildren,
+	use,
+	useEffect,
+	useState,
 } from "react";
-import { ClientRouter } from "@100x/router";
+import { ClientRouter, navigationEvent } from "@100x/router";
 import { nonNullOrThrow } from "@100x/engine/asserts";
 import { useWorld } from "./ecs";
-import { navigationEvent } from "../../router/.build/client";
 
 export const routerContext = createContext<ClientRouter<any, any> | null>(null);
 
 export const useRouter = () => {
-  const world = useWorld()?.resolveComponent(ClientRouter);
-  const routerCtx = use(routerContext);
-  const router = nonNullOrThrow(
-    world ?? routerCtx,
-    "useRouter must be used within a RouterProvider or world with ClientRouter",
-  );
+	const world = useWorld()?.resolveComponent(ClientRouter);
+	const routerCtx = use(routerContext);
+	const router = nonNullOrThrow(
+		world ?? routerCtx,
+		"useRouter must be used within a RouterProvider or world with ClientRouter",
+	);
 
-  const [routerState, setRouterState] = useState(() =>
-    createRouterData(router),
-  );
+	const [routerState, setRouterState] = useState(() =>
+		createRouterData(router),
+	);
 
-  useEffect(
-    () =>
-      router.register(navigationEvent, () => {
-        setRouterState(createRouterData(router));
-      }),
-    [router],
-  );
+	useEffect(
+		() =>
+			router.register(navigationEvent, () => {
+				setRouterState(createRouterData(router));
+			}),
+		[router],
+	);
 
-  return routerState;
+	return routerState;
 };
 
 const createRouterData = (r: ClientRouter<any, any>) => ({
-  get url() {
-    return r.url;
-  },
-  get searchParams() {
-    return r.searchParams;
-  },
-  get pathname() {
-    return r.pathname;
-  },
-  get hash() {
-    return r.hash;
-  },
-  get href() {
-    return r.href;
-  },
-  get router() {
-    return r;
-  },
+	get url() {
+		return r.url;
+	},
+	get searchParams() {
+		return r.searchParams;
+	},
+	get pathname() {
+		return r.pathname;
+	},
+	get hash() {
+		return r.hash;
+	},
+	get href() {
+		return r.href;
+	},
+	get router() {
+		return r;
+	},
 });
 
 export const RouterProvider = (
-  props: PropsWithChildren<{ router: ClientRouter<any, any> }>,
+	props: PropsWithChildren<{ router: ClientRouter<any, any> }>,
 ) =>
-  createElement(
-    routerContext.Provider,
-    { value: props.router },
-    props.children,
-  );
+	createElement(
+		routerContext.Provider,
+		{ value: props.router },
+		props.children,
+	);
--- react/src/selector.ts
+++ react/src/selector.ts
@@ -1,4 +1,4 @@
-import {useRef} from "react";
+import { useRef } from "react";
 
 /**
  * A React hook that provides a proxy for managing selectors of HTML elements.
@@ -8,56 +8,56 @@ import {useRef} from "react";
  * return (<div ref={parent}><span ref={child}>Hello</span></div>);
  */
 export function useSelectors<T extends HTMLElement>() {
-    const map = useRef(new Map<PropertyKey, T>());
-    const proxy = useRef(new Proxy(map.current, useSelectorProxyHandler));
-    return proxy.current as unknown as SelectorProxy<T>;
+	const map = useRef(new Map<PropertyKey, T>());
+	const proxy = useRef(new Proxy(map.current, useSelectorProxyHandler));
+	return proxy.current as unknown as SelectorProxy<T>;
 }
 
 const useSelectorProxyHandler: ProxyHandler<Map<PropertyKey, any>> = {
-    get(target, name) {
-        if (name === Symbol.iterator) {
-            return target.values.bind(target);
-        }
-        function handler(thing?: unknown) {
-            if (thing === null) {
-                target.delete(name);
-            } else {
-                target.set(name, thing);
-            }
-        }
-        const proxy = new Proxy(handler, {
-            get(_, prop) {
-                if (prop === "orNull") {
-                    return () => target.get(name) ?? null;
-                }
-                if (prop === "orUndefined") {
-                    return () => target.get(name);
-                }
-                if (prop === "notNull") {
-                    return () => target.get(name) ?? null;
-                }
-                return Reflect.get(handler, prop);
-            },
-            apply(
-                target: (thing?: unknown) => void,
-                thisArg: any,
-                argArray: any[],
-            ): any {
-                return Reflect.apply(target, thisArg, argArray);
-            },
-        });
-        return proxy;
-    },
+	get(target, name) {
+		if (name === Symbol.iterator) {
+			return target.values.bind(target);
+		}
+		function handler(thing?: unknown) {
+			if (thing === null) {
+				target.delete(name);
+			} else {
+				target.set(name, thing);
+			}
+		}
+		const proxy = new Proxy(handler, {
+			get(_, prop) {
+				if (prop === "orNull") {
+					return target.get(name) ?? null;
+				}
+				if (prop === "orUndefined") {
+					return target.get(name);
+				}
+				if (prop === "notNull") {
+					return target.get(name) ?? null;
+				}
+				return Reflect.get(handler, prop);
+			},
+			apply(
+				target: (thing?: unknown) => void,
+				thisArg: any,
+				argArray: any[],
+			): any {
+				return Reflect.apply(target, thisArg, argArray);
+			},
+		});
+		return proxy;
+	},
 };
 
 type SelectorProxy<T> = {
-    [Symbol.iterator]: Map<string, T>["values"];
+	[Symbol.iterator]: Map<string, T>["values"];
 } & Record<
-    string,
-    {
-        (element: T | null): VoidFunction;
-        orNull: T | null;
-        orUndefined: T | undefined;
-        notNull: T;
-    }
+	string,
+	{
+		(element: T | null): VoidFunction;
+		orNull: T | null;
+		orUndefined: T | undefined;
+		notNull: T;
+	}
 >;
